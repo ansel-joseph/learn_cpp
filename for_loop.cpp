efaac95baf2a1ca7f7507c6ee6e73c8166a2992b
Ansel Joseph <anseljoseph2509@gmail.com>
2024-10-29 16:29:55 +0530

    style: formatted

--- for_loop.cpp
+++ for_loop.cpp
@@ -1,11 +1,11 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-
 int main()
 {
-    for (int i = 1; i <= 10; i++)
-    {
-        cout << i << "\n";
+    for(int i=1; i<=10; i++){
+         cout<<i<<endl;
     }
+   
     return 0;
+    
 }
