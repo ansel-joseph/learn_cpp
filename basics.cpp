9d7e4ecef73633b38160ea1c134918070d2a2c35
Ansel Joseph <anseljoseph2509@gmail.com>
2024-10-31 18:36:15 +0530

    style: traffic lights

--- basics.cpp
+++ basics.cpp
@@ -3,27 +3,42 @@ using namespace std;
 int main()
 {
     char colour;
+
+while (true)
+    {
+      switch(colour);  
+
     cout << "Enter a colour";
     cin >> colour;
 
+    
+       
     switch (colour)
     {
-
+       
     case 'r':
-        cout << "STOP";
+        cout << "STOP"<<endl;
         break;
 
     case 'y':
-        cout << "WAIT";
+        cout << "WAIT"<<endl;
         break;
 
     case 'g':
-        cout << "GO";
+        cout << "GO"<<endl;
         break;
+        
+    case 's':
+        cout<<"GoodBye"<<endl;
+        return 0;
 
     default:
-        cout << "Invalid Input";
+        cout << "Invalid Input"<<endl;
     }
+
+    }
+
+        cout<<"Bye";
 
     return 0;
 }
