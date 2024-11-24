577d3efeeb9ff054a655c07f48a11cadaa3cef5c
Ansel Joseph <anseljoseph2509@gmail.com>
2024-11-24 12:06:05 +0530

    add: structure

--- functions.cpp
+++ functions.cpp
@@ -1,16 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+struct Book{
+    char title[50];
+    float price;
+    int year;
+};
+int main(){
+    Book b1;
+ cout<<"Enter the book name: ";
+ cin>>b1.title;
+ cout<<"Enter the Price of the book: ";
+ cin>>b1.price;
+ cout<<"Enter the Publication Year: ";
+ cin>>b1.year;
 
-    int* age = new int;
-
-    cout << "Enter the student's age: ";
-    cin >> *age;
-
-    cout << "The student's age is: " << *age << endl;
-
-    delete age;
+ cout<<"Book Information: "<<endl;
+ cout<<"Name of the Book: "<<b1.title<<endl;
+ cout<<"Price of the Book: "<<b1.price<<endl;
+ cout<<"Publication Year: "<<b1.year<<endl;
 
     return 0;
 }
