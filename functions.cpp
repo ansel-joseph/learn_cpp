6c612c1cc71e017d6bbafa91905c966ab91a4910
Ansel Joseph <anseljoseph2509@gmail.com>
2024-11-24 15:53:55 +0530

    add: inline function

--- functions.cpp
+++ functions.cpp
@@ -1,24 +1,16 @@
 #include <iostream>
 using namespace std;
 
-struct Book{
-    char title[50];
-    float price;
-    int year;
-};
+inline int areaRect(int length, int breadth){
+    return length*breadth;
+}
 int main(){
-    Book b1;
- cout<<"Enter the book name: ";
- cin>>b1.title;
- cout<<"Enter the Price of the book: ";
- cin>>b1.price;
- cout<<"Enter the Publication Year: ";
- cin>>b1.year;
+    int l,b;
+    cout<<"Enter the length of the rectangle: ";
+    cin>>l;
+    cout<<"Enter the breadth of the rectangle: ";
+    cin>>b;
 
- cout<<"Book Information: "<<endl;
- cout<<"Name of the Book: "<<b1.title<<endl;
- cout<<"Price of the Book: "<<b1.price<<endl;
- cout<<"Publication Year: "<<b1.year<<endl;
-
+    cout<<"Area of the rectangle is: "<<areaRect(l,b);
     return 0;
 }
