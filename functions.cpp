9ec3e29291d3702a65c5206d132ecbc63a95c3b7
Ansel Joseph <anseljoseph2509@gmail.com>
2024-11-25 23:33:58 +0530

    add: factorial using recursion

--- functions.cpp
+++ functions.cpp
@@ -1,24 +1,16 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-
-
-void fightPain(int remainingPains) {
-
-    if (remainingPains == 0) {
-        cout << "Naruto defeats Nagato!" << endl;
-        return;
+int factorial(int n){
+    if(n<=1){
+        return 1;
+    }else{
+        return n*factorial(n-1);
     }
-    
-    cout << "Naruto fights Pain number " << remainingPains << endl;
-
-    fightPain(remainingPains - 1);
 }
-
-int main() {
-    int totalPains = 6;  
-    
-    cout << "Naruto begins his mission to fight the Pains!" << endl;
-    fightPain(totalPains);
-    
+int main(){
+    int num;
+    cout<<"Enter any number: ";
+    cin>>num;
+    cout<<"The factorial is "<<factorial(num)<<endl;
     return 0;
 }
