f8ad8e0b66327fccff3a0ffa1efed0969d5a2bdc
Ansel Joseph <anseljoseph2509@gmail.com>
2024-11-26 19:01:36 +0530

    add: movie details using struct

--- functions.cpp
+++ functions.cpp
@@ -1,22 +1,27 @@
 #include <iostream>
 using namespace std;
 
-int sumDigits(int a) {
-    
-    if (a == 0) {
-        return 0;
-    }
-    return (a % 10) + sumDigits(a / 10);
-}
+struct Movie{
+    char title[50];
+    char director[50];
+    int releaseYear;
+    float rating;
+};
+int main(){
+    Movie m;
+    cout<<"Enter the movie name: ";
+    cin>>m.title;
+    cout<<"Enter the name of the Director: ";
+    cin>>m.director;
+    cout<<"Enter the year of release: ";
+    cin>>m.releaseYear;
+    cout<<"Etner the Rating: ";
+    cin>>m.rating;
 
-int main() {
-    int x;
-    cout << "Enter a number: ";
-    cin >> x;
-
-    int result = sumDigits(x);
-
-    cout << "The sum of digits is " << result << endl;
-
+    cout<<"\nMOVIE INFO: "<<endl;
+    cout<<"Movie Name: "<<m.title<<endl;
+    cout<<"Director: "<<m.director<<endl;
+    cout<<"Release Year: "<<m.releaseYear<<endl;
+    cout<<"Rating: "<<m.rating<<endl;
     return 0;
 }
