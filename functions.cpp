55ebfd6d213f08e5727391a6800e4dae07d6e07c
Ansel Joseph <anseljoseph2509@gmail.com>
2024-11-26 18:44:21 +0530

    add: sum of digits using recursion

--- functions.cpp
+++ functions.cpp
@@ -1,16 +1,22 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
-int factorial(int n){
-    if(n<=1){
-        return 1;
-    }else{
-        return n*factorial(n-1);
+
+int sumDigits(int a) {
+    
+    if (a == 0) {
+        return 0;
     }
+    return (a % 10) + sumDigits(a / 10);
 }
-int main(){
-    int num;
-    cout<<"Enter any number: ";
-    cin>>num;
-    cout<<"The factorial is "<<factorial(num)<<endl;
+
+int main() {
+    int x;
+    cout << "Enter a number: ";
+    cin >> x;
+
+    int result = sumDigits(x);
+
+    cout << "The sum of digits is " << result << endl;
+
     return 0;
 }
