f9a1e75aa2a4c94ca5e011850b8520d0e5bb5240
Ansel Joseph <anseljoseph2509@gmail.com>
2024-11-27 23:58:19 +0530

    add: square using functions

--- functions.cpp
+++ functions.cpp
@@ -1,27 +1,12 @@
 #include <iostream>
 using namespace std;
 
-struct Movie{
-    char title[50];
-    char director[50];
-    int releaseYear;
-    float rating;
-};
-int main(){
-    Movie m;
-    cout<<"Enter the movie name: ";
-    cin>>m.title;
-    cout<<"Enter the name of the Director: ";
-    cin>>m.director;
-    cout<<"Enter the year of release: ";
-    cin>>m.releaseYear;
-    cout<<"Etner the Rating: ";
-    cin>>m.rating;
+inline int square(int x) {
+    return x * x;
+}
 
-    cout<<"\nMOVIE INFO: "<<endl;
-    cout<<"Movie Name: "<<m.title<<endl;
-    cout<<"Director: "<<m.director<<endl;
-    cout<<"Release Year: "<<m.releaseYear<<endl;
-    cout<<"Rating: "<<m.rating<<endl;
+int main() {
+    int num = 5;
+    cout << "The square of " << num << " is " << square(num) << endl;
     return 0;
 }
