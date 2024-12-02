dccd5dd9a31fd0c192c6dad17beb1cb24a4fe9b0
Ansel Joseph <anseljoseph2509@gmail.com>
2024-12-02 11:36:14 +0530

    add: sum of first n even numbers

--- loops.cpp
+++ loops.cpp
@@ -5,9 +5,9 @@
       int sum=0;
       cout<<"Enter a number: ";
       cin>>n;
-      for(int i=1; i<=n; i++){
+      for(int i=2; i<=n; i+=2){
         sum+=i;
       }
-      cout<<"The sum of the first "<<n<<" numbers is: "<<sum;
+      cout<<"The sum of the first "<<n<<" even numbers is: "<<sum;
         return 0;
     }
