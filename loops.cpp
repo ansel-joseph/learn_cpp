4a8fe943dec38baa5fd56744aa1da5838ec58643
Ansel Joseph <anseljoseph2509@gmail.com>
2024-12-31 23:55:36 +0530

    add: sum of matrix

--- loops.cpp
+++ loops.cpp
@@ -1,12 +1,32 @@
 #include<iostream>
 using namespace std;
 int main(){
-  
-  cout<<"Tha Pattern is: "<<endl;
-  for (int i = 5; i >=1; i--)
-  {
-    for(int j = 1; j<=i; j++){
-      cout<<"*";
+  int rows,cols,matrix1[10][10],matrix2[10][10],sum[10][10];
+  cout<<"Enter the number of rows: ";
+  cin>>rows;
+  cout<<"Enter the number of columns: ";
+  cin>>cols;
+  cout<<"Enter the elements of the first matrix: "<<endl;
+  for(int i=0; i<rows; i++){
+    for(int j=0; j<cols; j++){
+      cin>>matrix1[i][j];
+    }
+  }
+  cout<<"Enter the elements for the second matrix: "<<endl;
+  for(int i=0; i<rows; i++){
+    for(int j=0; j<cols; j++){
+      cin>>matrix2[i][j];
+    }
+  }
+  for(int i=0; i<rows; i++){
+    for(int j=0; j<cols; j++){
+      sum[i][j]=matrix1[i][j]+matrix2[i][j];
+    }
+  }
+    cout<<"Sum matrix: "<<endl;
+    for(int i=0; i<rows; i++){
+    for(int j=0; j<cols; j++){
+      cout<<sum[i][j]<<" ";
     }
     cout<<endl;
   }
