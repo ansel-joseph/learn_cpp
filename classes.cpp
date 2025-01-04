3293f6c20e01a73ccedff83ae1f6f3c718a5b6ad
Ansel Joseph <anseljoseph2509@gmail.com>
2025-01-04 12:15:45 +0530

    add: class student details

--- classes.cpp
+++ classes.cpp
@@ -1,24 +1,36 @@
 #include <iostream>
 using namespace std;
-class Car{
+class Student{
     private:
-    int num;
-    char brand[50];
+    char name[100];
+    int rollNo;
+    int year;
+    char programme [100];
     public:
     void putData(){
-        cout<<"Enter the name of the car: ";
-        cin>>brand;
-        cout<<"Enter the number of your car: ";
-        cin>>num;
+        cout<<"Enter the name of the student: ";
+        cin>>name;
+        cout<<"Enter the Roll Number: ";
+        cin>>rollNo;
+        cout<<"Enter the year of joining: ";
+        cin>>year;
+        cout<<"Enter the programme taken";
+        cin>>programme;
     }
     void displayData(){
-        cout<<"The car is a "<<brand<<endl;
-        cout<<"The number of the car is: "<<num<<endl;
+        cout<<"---STUDENT INFORMATION---"<<endl;
+        cout<<"Name           : "<<name<<endl;
+        cout<<"Roll Number    : "<<rollNo<<endl;
+        cout<<"Year of joining: "<<year<<endl;
+        cout<<"Programme taken: "<<programme<<endl;
+
     }
+
 };
+
 int main(){
-    Car car;
-    car.putData();
-    car.displayData();
+    Student s;
+    s.putData();
+    s.displayData();
     return 0;
 }
