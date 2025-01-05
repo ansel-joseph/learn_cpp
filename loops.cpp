4029b3438a42ef542b79059bbd8b7585c362785e
Ansel Joseph <anseljoseph2509@gmail.com>
2025-01-05 18:45:15 +0530

    add: prime numbers

--- loops.cpp
+++ loops.cpp
@@ -1,34 +1,17 @@
 #include<iostream>
 using namespace std;
 int main(){
-  int rows,cols,matrix1[10][10],matrix2[10][10],sum[10][10];
-  cout<<"Enter the number of rows: ";
-  cin>>rows;
-  cout<<"Enter the number of columns: ";
-  cin>>cols;
-  cout<<"Enter the elements of the first matrix: "<<endl;
-  for(int i=0; i<rows; i++){
-    for(int j=0; j<cols; j++){
-      cin>>matrix1[i][j];
+  cout<<"Prime numbers from 1 to 100 are :"<<endl;
+  for(int i=2; i<=100; i++){
+    for(int j=2; j<=i; j++){
+      if(i%j==0){
+        break;
+      }
+    }
+    if(int j==i){
+      cout<<i<<endl;
     }
   }
-  cout<<"Enter the elements for the second matrix: "<<endl;
-  for(int i=0; i<rows; i++){
-    for(int j=0; j<cols; j++){
-      cin>>matrix2[i][j];
-    }
-  }
-  for(int i=0; i<rows; i++){
-    for(int j=0; j<cols; j++){
-      sum[i][j]=matrix1[i][j]+matrix2[i][j];
-    }
-  }
-    cout<<"Sum matrix: "<<endl;
-    for(int i=0; i<rows; i++){
-    for(int j=0; j<cols; j++){
-      cout<<sum[i][j]<<" ";
-    }
-    cout<<endl;
-  }
-  return 0;
+cout<<endl;
+return 0;
 }
