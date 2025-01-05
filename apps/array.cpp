eec3f5ca047917ac5c45d5fdd2986361cbfc4d5a
Ansel Joseph <anseljoseph2509@gmail.com>
2025-01-06 00:07:11 +0530

    add: linear search

--- apps/array.cpp
+++ apps/array.cpp
@@ -1,14 +1,33 @@
 #include <iostream>
 using namespace std;
-int main(){
-    int arr[5]={1,2,3,4,5};
-    cout<<"The array in first position is: "<<arr[0]<<endl;
-    arr[0]=6;
-    cout<<"After modification, array in the first position is: "<<arr[0]<<endl;
-    for (int i = 0; i < 5; i++)
-    {
-        cout<<"Array is: "<<arr[i];
-        cout<<endl;
+
+int main() {
+    int size, target, f = 0;  
+
+    cout << "Enter the number of elements in the array: ";
+    cin >> size;
+    
+    int arr[size];  
+
+    cout << "Enter the elements of the array: ";
+    for (int i = 0; i < size; i++) {
+        cin >> arr[i];
+    }
+
+    cout << "Enter the number to search for: ";
+    cin >> target;
+
+    // Linear search
+    for (int i = 0; i < size; i++) {
+        if (arr[i] == target) {
+            cout << "Element " << target << " found at index " << i << endl;
+            f = 1;  
+            break;  
+        }
+    }
+
+    if (f == 0) {
+        cout << "Element " << target << " not found in the array." << endl;
     }
 
     return 0;
