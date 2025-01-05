5c65b3b7ff5b67782698f89e5de364298a4ff281
Ansel Joseph <anseljoseph2509@gmail.com>
2025-01-06 00:07:24 +0530

    add: armstrong numbers

--- apps/calc.cpp
+++ apps/calc.cpp
@@ -1,36 +1,28 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 
-class Armstrong{
-    private: 
-    int n, original, sum=0;
-    public:
-    void display(){
-        cout<<"Enter an armstrong number: ";
-        cin>>n;
-    }
-    void calculate(){
-        original=n;
-    while (n>0)
-    {
-        int digit=n%10;
-        sum+=digit*digit*digit;
-        n/=10;
-        
-    }
-    if(original==sum){
-        cout<<original<<" is an armstrong number";
-    }else{
-        cout<<"It is not an armstrong number";
-    }
-    }
-};
 int main() {
-    Armstrong a;
-    a.display();
-    a.calculate();
+    int i, original, digit, sum;
+
+    cout << "Armstrong numbers between 1 and 1000 are: " << endl;
     
-    
+
+    for(i = 1; i <= 1000; i++) {
+        original = i;
+        sum = 0;
+        
+        
+        while (original > 0) {
+            digit = original % 10;     
+            sum += digit * digit * digit; 
+            original /= 10;            
+        }
+        
+
+        if (sum == i) {
+            cout << i << " ";
+        }
+    }
 
     return 0;
 }
